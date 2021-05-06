f41f932e5c2268eb86a46cef293c51d95ab83042
Ilya Sarantsev <ilya.sarantsev@gmail.com>
2021-05-06 18:36:08 +0300

    Update easy 1

--- src/easy1.ts
+++ src/easy1.ts
@@ -3,7 +3,7 @@
 // Нужно заменить FIXME на тип который вычисляется на освове OrderState
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = Array<"initial" | "inWork" | "fullfilled">;
+type FIXME = Array<OrderState>;
 
 const orderStates = [
     "initial",
